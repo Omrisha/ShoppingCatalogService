66ebba66c1ad89523b356094a140886debc30d6e
Omri Shapira <omris11@mail.afeka.ac.il>
2020-11-29 16:23:08 +0200

    Adding more boudaries + entites and dao

--- src/main/java/il/ac/afeka/shoppingcatalogservice/ShoppingCatalogService.java
+++
@@ -1,8 +0,0 @@
-package il.ac.afeka.shoppingcatalogservice;
-
-public interface ShoppingCatalogService {
-
-    void createCategory(CategoryBoundary value);
-
-    ProductBoundary createProduct(ProductBoundary value);
-}
