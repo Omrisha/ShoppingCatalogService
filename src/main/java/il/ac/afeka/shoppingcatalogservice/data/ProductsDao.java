7b53fddc96f8f431735ab7780d8e5fdada076dec
danielsason112 <danielsason112@gmail.com>
2020-11-30 21:26:06 +0200

    Added products search

--- src/main/java/il/ac/afeka/shoppingcatalogservice/data/ProductsDao.java
+++ src/main/java/il/ac/afeka/shoppingcatalogservice/data/ProductsDao.java
@@ -1,7 +1,17 @@
 package il.ac.afeka.shoppingcatalogservice.data;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductsDao extends JpaRepository<ProductEntity, String> {
 
+	List<ProductEntity> findByName(String name, Pageable pageable);
+	
+	List<ProductEntity> findByPriceIsGreaterThanEqual(double price, Pageable pageable);
+	
+	List<ProductEntity> findByPriceIsLessThanEqual(double price, Pageable pageable);
+
+	List<ProductEntity> findByCategoryName(String categoryName, Pageable pageable);
 }
